53ba8d2355cc0af4a15357ba41d8449185982197
thefreeman <thfreeman@bh.com>
2012-06-11 11:00:00 +0400

    Add DebugUI and DebugMonitor to track Player Coordinates, Waypoint class
    Initial commit to setup basic Monitor and UI for debug class.
    Currently runs a .5 second loop to update two text boxes with
    player coordinates.  Hitting Add will create a D3.Point object and
    add it to lstCoordinates.
    
    Added simple Waypoint class, storing a point, level area, quest id
    and quest step.  These are created and added to the listbox.
    
    Implemented the save button which uses an XMLSerializer to save
    the waypoints stored in the list.

---
+++ Snowshoes/Common/DebugMonitor.cs
@@ -0,0 +1,45 @@
+﻿#region
+
+using System;
+using D3;
+
+#endregion
+
+namespace Snowshoes.Common
+{
+    internal class DebugMonitor : Sherpa
+    {
+        private float _x;
+        private float _y;
+        private SNOQuestId _questId;
+        private int _questStep;
+        private SNOLevelArea _levelArea;
+        private UI.DebugUI _debugWindow;
+
+        public DebugMonitor(int delay, UI.DebugUI debugWindow) : base(delay)
+        {
+            ImmuneToPause = true;
+            _debugWindow = debugWindow;
+        }
+
+        protected override void Loop()
+        {
+            if (GetBool(updateData))
+            {
+                _debugWindow.txtX.Text = _x.ToString();
+                _debugWindow.txtY.Text = _y.ToString();
+            }
+        }
+
+        private bool updateData()
+        {
+            bool changed = changed = (_x != Me.X || _y != Me.Y);
+            _x = Me.X;
+            _y = Me.Y;
+            _levelArea = Me.LevelArea;
+            _questId = Me.QuestId;
+            _questStep = Me.QuestStep;
+            return changed;
+        }
+    }
+}
